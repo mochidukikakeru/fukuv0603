48041e8aa2e7c10a1c9053bbf03be7b29d0b34be
A601-22 <tnk.dat.class@gmail.com>
2021-06-03 09:33:37 +0900

    fuku

--- fukuv0603/Form1.cs
+++ fukuv0603/Form1.cs
@@ -17,5 +17,15 @@ namespace fukuv0603
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           label1 .Left += int.Parse(textBox1.Text);
+           label1.Top += int.Parse(textBox2.Text);
+        }
     }
 }
